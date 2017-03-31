0a32335a39bcbcadb18b6c0a8bfd2d543910d9dc
Devon Hogencamp <devhogen1@gmail.com>
2017-03-31 15:05:08 -0700

    ejs

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 import config from './config';
-import fs from 'fs';
+import apiRouter from './api/index';
+import path from 'path';
 
 import express from 'express';
 
 const server = express();
 
+server.set('views', path.join(__dirname + 'views'));
+server.set('view engine', 'ejs');
+
 server.get('/', (req, res) => {
-    res.send('Hellow Express');
+    res.send('Hello Express');
 });
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
 
 server.listen(config.port, () => {
     console.info('Server listening on localhost:' + config.port);
